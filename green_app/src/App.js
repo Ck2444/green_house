e637dea4513c2f61e309be33fec406735650821e
Konstantin Evstigneev <t777tt88@gmail.com>
2024-11-20 13:52:32 +0100

    header and floor

--- green_app/src/App.js
+++ green_app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Floor from './components /Floor/Floor';
+import Header from './components /Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Все работает!</h1>
       <h1>exemple</h1>
-      
+
+      <Header />
+      <Floor />
     </div>
   );
 }
---
+++ green_app/src/components /Floor/Floor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Floor() {
+  return (
+    <>
+      <h1>This is Floor compontent</h1>
+    </>
+  );
+}
